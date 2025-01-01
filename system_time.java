6b95cf9a8778e0c1aa51110fe82215b07cb75950
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-01 15:18:40 +0530

    Add files via upload

---
+++ system_time.java
@@ -0,0 +1,7 @@
+public class system_time {
+
+    public static void main(String[] args) {
+        // Display the current date and time using System.currentTimeMillis()
+        System.out.format("\nCurrent Date time: %tc%n\n", System.currentTimeMillis());
+    }
+}
