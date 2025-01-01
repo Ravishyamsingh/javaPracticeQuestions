8edcbccdbda2434182975bcc4f599cdadf007569
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-01 15:17:50 +0530

    Add files via upload

---
+++ print1to100divided.java
@@ -0,0 +1,25 @@
+public class print1to100divided {
+    public static void main(String args[]) {
+        // Print numbers divided by 3
+        System.out.println("\nDivided by 3: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0)
+                System.out.print(i + ", ");
+        }
+
+        // Print numbers divided by 5
+        System.out.println("\n\nDivided by 5: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 5 == 0)
+                System.out.print(i + ", ");
+        }
+
+        // Print numbers divided by both 3 and 5
+        System.out.println("\n\nDivided by 3 & 5: ");
+        for (int i = 1; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.print(i + ", ");
+        }
+        System.out.println("\n");
+    }
+}
