e2187b9b1796092ae1277b2a10352be61e2ca41d
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-07 23:26:47 +0530

    Add files via upload

---
+++ firstchareachstring.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class firstchareachstring {
+    public static void main(String[] args) {
+        // Define two strings
+        String str1 = "Python";
+        String str2 = "Tutorial";
+
+        // Print the substrings of both strings, excluding the first character
+        System.out.println(str1.substring(1) + str2.substring(1));
+    }
+}
