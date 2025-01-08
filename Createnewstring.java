ed1976aaaf3bc6f404ba172af1c906dca43adb98
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-08 21:13:44 +0530

    Add files via upload

---
+++ Createnewstring.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+public class Createnewstring {
+    public static void main(String[] args) {
+        // Define an empty string
+        String str1 = "";
+
+        // Get the length of the string
+        int len = str1.length();
+
+        // Check the length of the string and take different actions based on its length
+        if (len >= 3) {
+            // If the string has three or more characters, print the first three characters
+            System.out.println(str1.substring(0, 3));
+        } else if (len == 1) {
+            // If the string has only one character, add "##" to it and print
+            System.out.println(str1.charAt(0) + "##");
+        } else {
+            // If the string is empty or has two characters, print "###"
+            System.out.println("###");
+        }
+    }
+}
