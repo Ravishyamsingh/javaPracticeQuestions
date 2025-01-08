49cf279d7b18619cb49f2cee218905b54c6f9729
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-08 21:18:01 +0530

    Add files via upload

---
+++ FirstLastinArray.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class FirstLastinArray {
+    public static void main(String[] args) {
+        // Define an integer array
+        int[] num_array = {10, -20, 0, 30, 40, 60, 10};
+
+        // Check if the first or last element of the array is equal to 10
+        boolean result = (num_array[0] == 10 || num_array[num_array.length - 1] == 10);
+
+        // Print the result
+        System.out.println(result);
+    }
+}
