4a3db7887171a8544ec62a7f309d30c28dccd8a0
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-14 23:37:18 +0530

    Add files via upload

---
+++ Environment_Variables.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+public class Environment_Variables {
+    public static void main(String[] args) {
+        // Display the value of the specified environment variable "PATH"
+        System.out.println("\nEnvironment variable PATH: ");
+        System.out.println(System.getenv("PATH"));
+
+        // Display the value of the specified environment variable "TEMP"
+        System.out.println("\nEnvironment variable TEMP: ");
+        System.out.println(System.getenv("TEMP"));
+
+        // Display the value of the specified environment variable "USERNAME"
+        System.out.println("\nEnvironment variable USERNAME: ");
+        System.out.println(System.getenv("USERNAME"));
+    }
+}
