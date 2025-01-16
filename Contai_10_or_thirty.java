4f93841401113b9a292c5d33fdebdef6946fc3d0
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-01-16 21:31:07 +0530

    Add files via upload

---
+++ Contai_10_or_thirty.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Contai_10_or_thirty {
+    public static void main(String[] args) {
+        int[] array_nums = {11, 11, 13, 31, 45, 20, 33, 53};
+        int result = 1;
+        System.out.println("Original Array: "+Arrays.toString(array_nums));
+
+        // Iterate through the array
+        for(int i = 0; i < array_nums.length; i++) {
+            if(array_nums[i] == 10 || array_nums[i] == 30) {
+                // If the element is equal to 10 or 30, set the result to 0
+                result = 0;
+            }
+        }
+
+        if (result == 1)
+            System.out.printf(String.valueOf(false)); // If the result is 1, print false
+        else
+            System.out.printf(String.valueOf(true)); // If the result is not 1, print true
+    }
+}
