39b4a71c759d84c2086ded40c2131eae2a52d703
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-02-17 21:43:45 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please enter roll number: ");
+        int rollNo=input.nextInt();
+        System.out.print("Your roll number is "+ rollNo);
+
+    }
+}
