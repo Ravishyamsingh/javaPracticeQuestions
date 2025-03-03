5c3a02bf30aaea6495f0840f2532821627a0dddb
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-03-03 20:37:03 +0530

    Add files via upload

---
+++ print_factor.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class print_factor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Factors of " + n + " are:");
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
