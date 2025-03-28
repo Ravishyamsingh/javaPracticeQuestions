2f3bbffa1fea06127df7d6ad30a06273b6f3e99f
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-03-28 22:17:15 +0530

    Update AddWithoutOperators.java

--- AddWithoutOperators.java
+++ AddWithoutOperators.java
@@ -14,7 +14,8 @@ public class AddWithoutOperators {
         while (y != 0) {
             int carry = x & y; // Calculate the carry by bitwise AND operation between x and y
             x = x ^ y; // Calculate the sum without considering the carry by bitwise XOR operation
-            y = carry << 1; // Calculate the carry for the next iteration by shifting it left by one position
+            y = carry << 1; 
+            // Calculate the carry for the next iteration by shifting it left by one position
         }
 
         System.out.print("Sum: " + x); // Print the sum of the two numbers
