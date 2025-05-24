f9af38af780f924aded7ed0a4225ed6204e72bd5
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-24 19:40:55 +0530

    Update Arr_Aft_Last_ten.java

--- Arr_Aft_Last_ten.java
+++ Arr_Aft_Last_ten.java
@@ -4,16 +4,21 @@ public class Arr_Aft_Last_ten{
     public static void main(String[] args) {
         int[] array_nums = {11, 10, 13, 10, 45, 20, 33, 53};
         int result = 0;
+        
         System.out.println("Original Array: "+Arrays.toString(array_nums));
 
         int l = array_nums.length - 1;
+        
         int[] new_array;
 
+        
         // Find the last occurrence of 10 in the array
+        
         while(array_nums[l] != 10)
             l--;
 
         // Create a new array with elements after the last 10
+        
         new_array = new int[array_nums.length - 1 - l];
         for(int i = l + 1; i < array_nums.length; i++)
             new_array[i - l - 1] = array_nums[i];
