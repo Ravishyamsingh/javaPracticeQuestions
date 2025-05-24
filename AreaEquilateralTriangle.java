494ae1c2978d21d9224654969957e0354ae7f509
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-24 19:38:27 +0530

    Update AreaEquilateralTriangle.java

--- AreaEquilateralTriangle.java
+++ AreaEquilateralTriangle.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class AreaEquilateralTriangle {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
+        
         double side,Area;
         System.out.println("Enter the side: ");
+
+        
         side=in.nextDouble();
-        Area=(Math.sqrt(3)/4)*Math.pow(side, 2);    //√3/4 × (side)²
+        
+        Area=(Math.sqrt(3)/4)*Math.pow(side, 2);  
+        
+        //√3/4 × (side)²
         System.out.println(Area);
 
     }
