bd225e48a24ffe927cf5412744b996f3ab34b009
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-24 19:39:48 +0530

    Update Arithmatic_operation.java

--- Arithmatic_operation.java
+++ Arithmatic_operation.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 
 public class Arithmatic_operation {
     public static void main(String[] args) {
+        
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         System.out.println("Enter operator");
+        
         char operator =in.next().charAt(0);
         if (operator=='+'){
             System.out.println(num1+num2);
@@ -15,14 +17,20 @@ public class Arithmatic_operation {
             System.out.println(num1-num2);
         }
         else if (operator=='*'){
+            
             System.out.println(num1*num2);
 
         } else if (operator== '/'){
             if(num2 !=0){
+                
                 System.out.println(num1/num2);
+                
             }else{
+                
                 System.out.println("Zero division error");
+                
             }
+            
         }else{
             System.out.println("Please enter correct input: ");
         }
