0a6214718ff335af7468818cf9b0a7bbb7e55b4d
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-05-25 23:56:51 +0530

    Update Ceaser_Cipher.java

--- Ceaser_Cipher.java
+++ Ceaser_Cipher.java
@@ -7,12 +7,15 @@ public class Ceaser_Cipher {
         for (int i=0; i < text.length(); i++)
         {
             char c = chars[i];
+            
             if (c >= 32 && c <= 127)
+                
             {
                 // Change base to make life easier, and use an
                 // int explicitly to avoid worrying... cast later
                 int x = c - 32;
                 x = (x + shift) % 96;
+                
                 if (x < 0)
                     x += 96; //java modulo can lead to negative values!
                 chars[i] = (char) (x + 32);
