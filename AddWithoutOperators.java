c02dfcf0e3f4066d82cfb0e07af045c15aae9c3c
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-05 06:53:37 +0530

    Update AddWithoutOperators.java

--- AddWithoutOperators.java
+++ AddWithoutOperators.java
@@ -1,18 +1,22 @@
 import java.util.*;
 import java.util.Scanner;
 
+
 public class AddWithoutOperators {
     public static void main(String[] arg) {
         int x, y; // Declare two integer variables, 'x' and 'y'
         Scanner in = new Scanner(System.in); // Create a Scanner object for user input
+        
 
         System.out.print("Input first number: "); 
+        
         // Prompt the user to input the first number
         x = in.nextInt(); // Read and store the first number from the user
 
         System.out.print("Input second number: "); // Prompt the user to input the second number
         y = in.nextInt(); // Read and store the second number from the user
 
+        
         while (y != 0) {
             int carry = x & y; // Calculate the carry by bitwise AND operation between x and y
             x = x ^ y; // Calculate the sum without considering the carry by bitwise XOR operation
