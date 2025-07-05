95bf32c717808dfc71dad56918a41e5416338c6c
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-05 06:54:47 +0530

    Update Add_subtract_digit.java

--- Add_subtract_digit.java
+++ Add_subtract_digit.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
+
 public class Add_subtract_digit {
     public static void main(String[] args) {
+
         
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        
         int n = sc.nextInt();
         sc.close();
         
