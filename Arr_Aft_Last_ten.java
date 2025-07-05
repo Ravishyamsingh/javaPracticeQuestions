c9c01c0047424d36f02ca6c0c572f2b2736a6380
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-05 06:55:11 +0530

    Update Arr_Aft_Last_ten.java

--- Arr_Aft_Last_ten.java
+++ Arr_Aft_Last_ten.java
@@ -2,10 +2,12 @@ import java.util.*;
 import java.util.*;
 public class Arr_Aft_Last_ten{
     public static void main(String[] args) {
+        
         int[] array_nums = {11, 10, 13, 10, 45, 20, 33, 53};
         int result = 0;
         
         System.out.println("Original Array: "+Arrays.toString(array_nums));
+        
 
         int l = array_nums.length - 1;
         
