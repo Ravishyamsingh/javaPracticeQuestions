303635fe8bb55ee9fe81f3dc28843fb35944f64e
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-25 21:50:22 +0530

    Update AreaOfRectangle.java

--- AreaOfRectangle.java
+++ AreaOfRectangle.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.*;
 public class AreaOfRectangle {
     public static void main(String [] args){
+        
         Scanner in=new Scanner(System.in);
         double length,width,Arearectangle;
         System.out.println("Enter the Length:");
