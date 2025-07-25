1a400a1d751a644933067d6d7481b50890baee83
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-07-25 21:48:40 +0530

    Update AreaEquilateralTriangle.java

--- AreaEquilateralTriangle.java
+++ AreaEquilateralTriangle.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class AreaEquilateralTriangle {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-
-
         
         
         double side,Area;
