e11c1a40af129a653feba218f398d89a740034bb
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-08-30 21:15:11 +0530

    Update Environment_Variables.java

--- Environment_Variables.java
+++ Environment_Variables.java
@@ -4,10 +4,13 @@ public class Environment_Variables {
     public static void main(String[] args) {
         // Display the value of the specified environment variable "PATH"
         System.out.println("\nEnvironment variable PATH: ");
+        
+        
         System.out.println(System.getenv("PATH"));
 
         // Display the value of the specified environment variable "TEMP"
         System.out.println("\nEnvironment variable TEMP: ");
+        
         System.out.println(System.getenv("TEMP"));
 
         // Display the value of the specified environment variable "USERNAME"
@@ -15,3 +18,4 @@ public class Environment_Variables {
         System.out.println(System.getenv("USERNAME"));
     }
 }
+
