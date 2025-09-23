683bb1e46c00c4795e41f174783c151e7ebccc15
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-09-23 21:31:47 +0530

    Update Simple_Interest.java

--- Simple_Interest.java
+++ Simple_Interest.java
@@ -6,11 +6,16 @@ public class Simple_Interest {
         System.out.println("Enter the principle / amount: ");
         double princple = in.nextFloat();
         System.out.println("Enter rate: ");
+        
         double rate = in.nextFloat();
+        
         System.out.println("Enter time: ");
+        
         double time = in.nextFloat();
+        
         double si= (princple*rate*time)/100;
 
         System.out.println(princple+si);
     }
 }
+
