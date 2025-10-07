9903ba446db972c8b7576ab0e5dfdb3b1f2c7ff9
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2025-10-07 21:38:38 +0530

    Refactor MinPathSumGrid for clarity and efficiency

--- MinPathSumGrid.java
+++ MinPathSumGrid.java
@@ -1,6 +1,3 @@
-
-
-
 import java.util.*;
 public class MinPathSumGrid {
     // Static method to find the minimum path sum in a 2D grid
@@ -39,3 +36,4 @@ public class MinPathSumGrid {
         System.out.println("Sum of all numbers along its path: " + minPathSum(grid));
     }
 }
+
