430e19e482c69d314737cb3d2effd445eefcd203
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-26 18:26:24 +0530

    Add files via upload

---
+++ rightsidetriangle.java
@@ -0,0 +1,26 @@
+package Array_ArrayList;
+
+public class rightsidetriangle{
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("*"+"  ");
+            }
+            System.out.println();
+        }
+//        int n=5;
+//        for(int i=1;i<=n;i++){
+//            for(int j=i;j<=n;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
+
+
