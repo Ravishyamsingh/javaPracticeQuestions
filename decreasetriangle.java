5392d36d8b77ec0f0d44fe149ef47e78ef717736
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-26 18:27:05 +0530

    Add files via upload

---
+++ decreasetriangle.java
@@ -0,0 +1,14 @@
+package Array_ArrayList;
+
+public class decreasetriangle {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+    }
+}
