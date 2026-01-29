7adca3ae44f8f5d8ccaa38a8677a3c2d21cd0628
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-29 20:46:54 +0530

    Add reverseNum class to reverse an integer

---
+++ reverseNum.java
@@ -0,0 +1,12 @@
+public class reverseNum {
+    public static void main(String[] args) {
+        int num= 28479;
+        int ans=0;
+        while (num > 0){
+            int rem=num%10;
+            num/=10;
+            ans= ans * 10+rem;
+        }
+        System.out.println(ans);
+    }
+}
