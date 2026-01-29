fdade70507f8035cb07a725d1641f90e4b7f34cd
rss coder <156085103+Ravishyamsingh@users.noreply.github.com>
2026-01-29 20:36:48 +0530

    Add files via upload

---
+++ evenindearray.java
@@ -0,0 +1,12 @@
+package Array_ArrayList;
+
+public class evenindearray {
+    public static void main(String[] args) {
+        int[] arr={8,4,45,3,5,7};
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
